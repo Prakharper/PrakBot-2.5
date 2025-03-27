64faa0aed19eb73ed0a6b7877895d8c90589eafd
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-27 17:43:32 -0400

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,44 +1,44 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
+let staff = `🚀 *EQUIPO DE AYUDANTES*
 🤖 *Bot:* ${global.botname}
 🪐 *Versión:* ${global.vs}
 
 • 𝐃𝐚𝐫𝐤𝐂𝐨𝐫𝐞
-🎩 *Rol:* Developer
-💛 *Número:* Wa.me/51968382008
+💙 *Rol:* Developer
+🚀 *Número:* 
 
 • 𝐃𝐞𝐬𝐭𝐫𝐨𝐲
-🍭 *Rol* Developer
-💛 *Numero:* Wa.me/584120346669
+🧿 *Rol* Developer
+💙 *Numero:* 
 
 • 𝐏𝐫𝐚𝐤 𝐇𝐚𝐫𝐩𝐞𝐫 
-🎩 *Rol:* Developer
-💛 *Número:* Wa.me/584241836217
+🚀 *Rol:* Developer
+💙 *Número:* Wa.me/584241836217
 
 • 𝐈𝐬𝐚𝐛𝐞𝐥 (isa)
-🍭 *Rol:* Mod
-💛 *Número:* Wa.me/529831715910
+💙 *Rol:* Mod
+🚀 *Número:* 
 
 
 • 𝐌𝐨𝐫𝐚𝐥𝐞𝐬
-🍭 *Rol:* Mod
-💛 *Numero:* Wa.me/573007796996
+🧿 *Rol:* Mod
+💙 *Numero:* 
 
 • 𝐃𝐞𝐲𝐥𝐢𝐧 
-🎩 *Rol:* Mod
-💛 *Número:* Wa.me/50488198573
+🚀 *Rol:* Mod
+💙 *Número:* 
 
 • 𝐎𝐫𝐥𝐚𝐧𝐝𝐨
-🍭 *Rol:* Mod
-💛 *Número:* Wa.me/522731590195
+🧿 *Rol:* Mod
+🚀 *Número:* 
 
 • 𝐒𝐄𝐈𝐒𝐇𝐈𝐑𝐎 - 𝐍𝐀𝐆𝐈
-🎩 *Rol:* Mod
-💛 *Número:* Wa.me/51920227615
+💙 *Rol:* Mod
+🚀 *Número:* 
 
 • 𝐀𝐍𝐆𝐄𝐋
-🍭 *Rol:* Mod
-💛 *Numero:* Wa.me/50368471855`
+🚀 *Rol:* Mod
+ 🧿 *Numero:* Wa.me/`
 await conn.sendFile(m.chat, 'https://files.catbox.moe/h094yg.jpg', 'brook.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
@@ -46,7 +46,7 @@ contextInfo: {
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: false,
-title: `🎩 STAFF OFICIAL🌟`,
+title: `🚀 STAFF OFICIAL🚀`,
 body: dev,
 mediaType: 1,
 sourceUrl: redes,
