c2dfbf1c7b84f84b2d2b9a2832e0d600db6b1ad5
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-28 17:02:15 -0400

    Actualizar main-menufun.js

--- plugins/main-menufun.js
+++ plugins/main-menufun.js
@@ -10,13 +10,13 @@ import fetch from 'node-fetch'
 const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 const defaultMenu = {
-  before: `“🎳¡Hola! *%name*, a qui esta mi menu de juegos"
+  before: `“🚀¡Hola! *%name*, a qui esta mi menu de juegos"
 
 *─ׄ─ׄ─⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׅ──ׄ*
 %readmore
 `.trimStart(),
   header: '┏━━⪩「 *_`%category`_* 」⪨',
-  body: '┃『🎮』 %cmd\n',
+  body: '┃『🚀』 %cmd\n',
   footer: '┗━━━━━━━━━━━━━━━━⪩\n',
   after: `${dev}`,
 }
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'fun': 'Diversion',
 }
-let img = 'https://files.catbox.moe/w3s79j.jpg'
+let img = 'https://files.catbox.moe/qa9494.jpg'
 
   try {
           // DEFAULT MENU
