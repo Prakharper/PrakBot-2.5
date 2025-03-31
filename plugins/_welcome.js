78e9aa6984a9bf36b3982bdc689ed0248cafb8b5
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-30 20:42:42 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,7 +7,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://i.ibb.co/mVzcY6yV/file.jpg';
+  let defaultImage = 'https://files.catbox.moe/ftey8u.jpg';
 
   if (chat.welcome) {
     let img;
